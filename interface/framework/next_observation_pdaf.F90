d36ef333249d658b9f8dc4cd53e7dc7660960581
Johannes Keller <jo.keller@fz-juelich.de>
2025-07-31 16:03:18 +0200

    bugfix: remove `da_interval` from CLM-mode
    ```
    next_observation_pdaf.F90(72): error #6405: The same named entity from
    different modules and/or program units cannot be referenced.
    [DA_INTERVAL]
    ```

--- interface/framework/next_observation_pdaf.F90
+++ interface/framework/next_observation_pdaf.F90
@@ -69,7 +69,6 @@ SUBROUTINE next_observation_pdaf(stepnow, nsteps, doexit, time)
   use mod_read_obs, ONLY: check_n_observationfile_da_interval
   use mod_read_obs, ONLY: check_n_observationfile_set_zero
   use clm_time_manager, only: get_nstep
-  use enkf_clm_mod, only: da_interval
   use enkf_clm_mod, only: clmupdate_tws
   use clm_varcon, only: set_averaging_to_zero, ispval
   IMPLICIT NONE
